223d27dc5351715933987beb334851fe1a58136f
mkdoherty <m.doherty.7@gmail.com>
2017-06-19 18:10:52 -0400

    changed position of mtext labels and removed candidatus from OTU labels

--- code/Figure5_responseREMwk6Xwk6.R
+++ code/Figure5_responseREMwk6Xwk6.R
@@ -1,4 +1,4 @@
-pdf("figures/Figure5_wk6Xwk6.pdf", height = 8, width = 7)
+pdf("figures/Figure5_wk6Xwk6.pdf", height = 10, width = 6)
 
 layout(matrix(c(1,2), 2,1, byrow = T))
 par(mar=c(3,3.5,2,1), mgp=c(2,0.5,0), oma=c(0,1.5,0,0))
@@ -9,7 +9,7 @@ plot(c(1,0),c(0,1), type='l', lty=2, xlim=c(1.01,0), ylim=c(-0.01,1.01), xaxs='i
 plot(otu_remWK6xWK6_roc, add=T, lwd=2, col='royalblue', lty=1)
 plot(otu_relrespWK6xWK6_roc, add=T, lwd=2, col='red', lty=1)
 legend('bottomright', c(sprintf('Remission : AUC=%.3f',otu_remWK6xWK6_roc$auc),sprintf('Response : AUC=%.3f',otu_relrespWK6xWK6_roc$auc)), lwd=2, col=c('royalblue','red'), bty='n', lty=1)
-mtext('A', at=1, side=3, line=0.1, font=2, cex=2)
+mtext('A', at=1.15, side=3, line=0.1, font=2, cex=2)
 
 cutoff <- coords(roc=otu_relrespWK6xWK6_roc, x='best', best.method='y', ret=c('threshold'))
 otu_relrespWK6xWK6_spec <- coords(otu_relrespWK6xWK6_roc, x=cutoff, input='thr', ret='spec')
@@ -31,7 +31,7 @@ no_abunds <-na.omit(no_abunds)
 yes_abunds <- otu_remWK6xWK6[otu_remWK6xWK6$REMISSwk6=='1', oturf_otus]/10000 + 1e-5
 yes_abunds<- na.omit(yes_abunds)
 
-par(mar=c(3, 14.5, 2, 1))
+par(mar=c(3, 12, 2, 1))
 plot(1, type="n", ylim=c(0,length(oturf_otus )*2), xlim=c(1e-5,1.5), log="x", ylab="", xlab="Week 6 Relative Abundance (%)", xaxt="n", yaxt="n")
 set.seed(32016)
 index <- 1
@@ -48,11 +48,12 @@ colnames(rf_tax) <- c('OTU')
 oturf_tax<-merge(rf_tax, tax, by='OTU')
 oturf_tax<-oturf_tax[match(rf_tax$OTU, oturf_tax$OTU),]
 oturf_tax<-oturf_tax$Label
+oturf_tax <- gsub("Candidatus_", "", oturf_tax)
 oturf_tax <- gsub("_", " ", oturf_tax)
 axis(2, at=seq(1,index-2,2), labels=oturf_tax, font = 3, las=1, line=0, tick=F, cex.axis=1)
 axis(1, at=c(1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0), label=c("0", "0.01", "0.1", "1", "10", "100"))
 legend('right', legend=c("Week 6 Active CD", "Week 6 Remission"), pch=c(21, 21), pt.bg=c("orange","royalblue1"), cex=1)
-mtext('B', at=5e-9, side=3, line=0.1, font=2, cex=2)
+mtext('B', at=3.3e-10, side=3, line=0.1, font=2, cex=2)
 
 dev.off()
 
