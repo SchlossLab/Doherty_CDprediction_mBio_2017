10b96170ecb159b892699d21633615dedccbdd8f
mkdoherty <m.doherty.7@gmail.com>
2017-08-08 15:05:51 -0400

    added Run_render_paper.R

---
+++ code/Run_render_paper.R
@@ -0,0 +1,17 @@
+### List of commands to render final pdf
+
+# Load needed library
+#This function loads and installs libraries if they are not already loaded by the user 
+loadLibs <- function(deps){
+  for (dep in deps){
+    if (dep %in% installed.packages()[,"Package"] == FALSE){
+      install.packages(as.character(dep), quiet=TRUE);
+    }
+    library(dep, verbose=FALSE, character.only=TRUE)
+  }
+}
+
+loadLibs(c("knitr", "rmarkdown"))
+
+# Render the final pdfs
+render('submission/Doherty_CDprediciton_mBio_2017_finaldraft.Rmd', clean = FALSE)
